2ac32eab5b1439cd52490db368639275309d154e
gaurav <gaurav@ggvaidya.com>
2011-11-21 23:41:48 -0700

    Updated HTTP 401 status code check to raise a CartoDBException instead of an Exception.

--- cartodb/cartodb.py
+++ cartodb/cartodb.py
@@ -66,7 +66,7 @@ class CartoDB(object):
         access_token_url = ACCESS_TOKEN_URL % {'user': cartodb_domain}
         resp, token = client.request(access_token_url, method="POST", body=urllib.urlencode(params))
         if resp['status'] == '401':
-            raise Exception("CartoDB username or password invalid: access denied.")
+            raise CartoDBException("CartoDB username or password invalid: access denied.")
         access_token = dict(urlparse.parse_qsl(token, False, True))
         token = oauth.Token(access_token['oauth_token'], access_token['oauth_token_secret'])
 
